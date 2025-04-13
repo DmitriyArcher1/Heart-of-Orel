f31164d949069be29d91330a4f86f9e5938aa23a
DmitriyArcher1 <d.kartrajt@gmail.com>
2025-04-13 15:08:40 +0300

    Добавлена функция удаления комментариев

--- comment/urls.py
+++ comment/urls.py
@@ -6,4 +6,5 @@ app_name = 'comment'
 
 urlpatterns = [
     path('first_place/', views.first_comments, name = 'comments'),
+    path('delete_comment/<int:comment_id>/', views.delete_comment, name='delete_comment'),
 ]
--- comment/views.py
+++ comment/views.py
@@ -5,7 +5,7 @@ from django.contrib import auth, messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Prefetch
 from django.http import HttpResponse, HttpResponsePermanentRedirect, HttpResponseRedirect
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse, reverse_lazy
 from django.views.generic import CreateView, TemplateView, UpdateView
 from django.core.cache import cache
@@ -27,8 +27,18 @@ def first_comments(request) -> HttpResponseRedirect | HttpResponsePermanentRedir
 
             return redirect(reverse('comment:comments'))
         else:
-            return render(request, 'comment/first_comments.html', {'form': form, 'comments': comments, 'title': 'Отзывы'})
+            return render(request, 'comment/first_comments.html', {'form': form, 'comments': comments, 'title': 'Отзывы', 'user': request.user})
     
     else:
         form = CommentForm()
-        return render(request, 'comment/first_comments.html', {'form': form, 'comments': comments, 'title': 'Отзывы'})
+        return render(request, 'comment/first_comments.html', {'form': form, 'comments': comments, 'title': 'Отзывы', 'user': request.user})
+    
+@login_required
+def delete_comment(request, comment_id) -> HttpResponseRedirect | HttpResponsePermanentRedirect:
+    comment = get_object_or_404(Comment, id = comment_id)
+
+    if comment.author == request.user or request.user.is_superuser:
+        comment.delete()
+        return redirect(reverse('comment:comments'))
+    else:
+        pass
